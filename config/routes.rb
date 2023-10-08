73c73eab257fb87ea26a881d879c65aa0e4a3dd8
osada1masaru <masaru5151su@icloud.com>
2023-10-08 17:23:28 +0900

    [add]bootstrapテンプレートファイル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :styles
+  get 'home/index'
+
+  get 'styles/index'
+
+  root to: "home#index"
+
+  devise_scope :user do
+    get '/users/sign_up' => 'devise/registrations#new'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  devise_for :users, controllers: {
+    registration: 'users/registration'
+  }
 end
