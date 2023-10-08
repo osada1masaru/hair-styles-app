bb95ace7ce654e4a082e530e9a24d3f332f91bf0
osada1masaru <masaru5151su@icloud.com>
2023-10-08 17:41:41 +0900

    [update]deviseコントローラー、モデル設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    # deviseコントローラーにストロングパラメータを追加する
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    def configure_permitted_parameters
+      # サインアップ時にnameのストロングパラメータを追加
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      # アカウント編集の時にnameとprofileのストロングパラメータを追加
+      devise_parameter_sanitizer.permit(:account_update, keys: %i(name image introdction))
+    end
+  
+    # private
+    def after_sign_in_path_for(_resourece)
+      home_index_path
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_one_attached :image
+
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
+
+  has_many :styles
+
+  validates :name, presence: true
 end
